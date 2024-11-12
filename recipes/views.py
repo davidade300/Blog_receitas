ce26251dad94c1dde35f59652bba83e8758361eb
davidade300 <iaderinho@icloud.com>
2024-11-12 16:36:23 -0300

    templates reestruturados, stilização adicionada ao arquivo head.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 
 
 def home(request):
-    return render(request, "recipes/home.html", {"name": "David"})
-
-
-def contato(request):
-    return HttpResponse("<h1>contato</h1>")
-
-
-def sobre(request):
-    return HttpResponse("<h1>sobre</h1>")
+    return render(request, "recipes/pages/home.html", {"name": "David"})
